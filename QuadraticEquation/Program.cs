a6fc08eb871c0c5edb0dfa248807ebe6ed7e6682
Natnael Habtemariam <natitam40@gmail.com>
2021-04-10 20:48:58 +0200

    Add .gitignore and .gitattributes.

---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        private static double A;
+        private static double B;
+        private static double C;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arrange the equation Ax^2+Bx+C = 0");
+            try
+            {
+                Console.WriteLine("Enter the coefficient of x-square (A)");
+                A = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the coefficient of x (B)");
+                B = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Constant (C)");
+                C = double.Parse(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Enter number only");
+            }
+            QuadraticEq equation = new QuadraticEq(A, B, C);
+            equation.ShowRoot();
+        }
+    }
+}
---
+++ QuadraticEquation/QuadraticEq.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuadraticEquation
+{
+    class QuadraticEq
+    {
+
+        private bool hasroot;
+        private bool PerfectSquare;
+
+        private double root1;
+        private double root2;
+        private double coefficient1;//coefficient of x-square
+        private double coefficient2;//coefficient of x
+        private double constant;//constant
+
+        public QuadraticEq(double coefficient1,double coefficient2,double constant)
+        {
+            this.coefficient1 = coefficient1;
+            this.coefficient2 = coefficient2;
+            this.constant = constant;
+        }
+       public void IsPerfectSquare()
+        {
+            
+            //check if the quadratic function has 1 root;  B^2-(4*A*C)=0
+            if (Math.Pow(coefficient2, 2) - (4 * coefficient1 * constant) == 0)
+            {
+                PerfectSquare = true;
+            }
+            else
+                PerfectSquare = false;
+        }
+        public void SolvePerfectSquare()
+        {
+            root1 = -coefficient2 /( 2 * coefficient1);
+        }
+        public void SolvenonPerfectSquare()
+        {
+            root1 = ((-coefficient2) + Math.Sqrt(Math.Pow(coefficient2, 2) - (4 * coefficient1 * constant))) / 2 * coefficient1;
+            root2 = ((-coefficient2) - Math.Sqrt(Math.Pow(coefficient2, 2) - (4 * coefficient1 * constant))) / 2 * coefficient1;
+        }
+        public void hasRoot()
+        {
+            if (Math.Pow(coefficient2, 2) - (4 * coefficient1 * constant) < 0)
+            {
+                hasroot= false;
+            }
+            else
+                hasroot= true;
+
+        }
+        public void ShowRoot()
+        {
+            hasRoot();
+            IsPerfectSquare();
+            if (hasroot)
+            {
+                if (PerfectSquare)
+                {
+                    SolvePerfectSquare();
+                    Console.WriteLine("The equation has 1 root : " + root1);
+                }
+                else
+                {
+                    SolvenonPerfectSquare();
+                    Console.WriteLine("The equation has 2 roots : " + root1 + " and " + root2);
+                }
+            }
+            else
+                Console.WriteLine("There are no real roots for the equation.");
+            
+            }
+    }
+}
